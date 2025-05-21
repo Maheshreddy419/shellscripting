0f5795449fb8d84d70bd55f636143b4c2f5b242f
Maheshreddy419 <mahesh.apple62@gmail.com>
2025-05-21 11:17:13 +0530

    shell commit

--- conversation.sh
+++ conversation.sh
@@ -7,3 +7,9 @@ echo "$PERSON1: Hi $PERSON2 How are you"
 echo "$PERSON2: Hi $PERSON1, I am Good What about you?"
 echo "$PERSON1: Thanks, I am good and how is your work going"
 echo "$PERSON2: Going well !!!! as of now"
+
+
+#second way
+echo "Enter you PIN Number:: "
+read -s PIN
+echo "Your PIN Number is:: $PIN"
