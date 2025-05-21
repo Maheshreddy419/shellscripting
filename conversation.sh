7e0d4cd3818a8ae7e68a81c59164eb10e081d285
Maheshreddy419 <mahesh.apple62@gmail.com>
2025-05-21 11:09:01 +0530

    shell commit

--- conversation.sh
+++ conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Jhon
-PERSON2=Mahesh
+PERSON1=$1    #there is no space before and after =
+PERSON2=$2
 
 echo "$PERSON1: Hi $PERSON2 How are you"
 echo "$PERSON2: Hi $PERSON1, I am Good What about you?"
