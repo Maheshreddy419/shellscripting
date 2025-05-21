3e47f3222c7e0a5b39a3e8a85eb024d94dce1c63
Maheshreddy419 <mahesh.apple62@gmail.com>
2025-05-21 10:59:52 +0530

    shell commit

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Jhon: Hi Mahesh How are you"
+echo "Mahesh: Hi Jhon, I am Good What about you?"
+echo "John: Thanks, I am good and how is your work going"
+echo "Mahesh: Going well !!!! as of now
